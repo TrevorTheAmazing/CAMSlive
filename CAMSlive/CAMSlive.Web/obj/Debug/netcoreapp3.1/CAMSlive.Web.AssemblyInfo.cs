2656515ba35b6ce2f19fb21f06bd15c580d62564
trevor clements <trevortheamazing@gmail.com>
2020-06-04 19:02:05 -0700

    progress.

--- CAMSlive/CAMSlive.Web/obj/Debug/netcoreapp3.1/CAMSlive.Web.AssemblyInfo.cs
+++ CAMSlive/CAMSlive.Web/obj/Debug/netcoreapp3.1/CAMSlive.Web.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("be1dd14b-d101-4b86-a8ec-0f391a248cfc")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("CAMSlive.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
---
+++ CAMSlive/CAMSlive.Web/obj/Release/netcoreapp3.1/win-x64/PubTmp/Out/wwwroot/js/scripts/main.js
@@ -0,0 +1,31 @@
+﻿//"use strict";
+
+async function RenderChart(chartId, chartOptions) {
+
+    let chart = new ApexCharts(document.getElementById(chartId), JSON.parse(chartOptions));
+
+    await chart.render();
+
+    console.log('end of RenderChart()');
+};
+
+async function UpdateOptions(chartId, chartOptions) {
+
+    //let chart = document.getElementById(chartId);
+
+    //let tempOptions = JSON.parse(chartOptions);
+
+    //let chartToUpdate = new ApexCharts(chart, tempOptions);
+
+    //await ApexCharts.exec(chart.id, "updateOptions", tempOptions, false, true);
+
+    let chart = window.ApexCharts.getChartByID(chartId);
+
+    let tempOptions = JSON.parse(chartOptions);
+
+    //chart.opts.series = tempOptions.series;
+    await chart.updateSeries(tempOptions.series);
+    //await ApexCharts.exec(chart.id, "updateOptions", tempOptions, false, true);
+    
+    console.log('end of UpdateOptions()');
+};
